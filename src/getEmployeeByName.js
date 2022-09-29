893716335c3a7328ad8a67f43d206993dbca7e0f
Bruna <brunaalmeida075@gmail.com>
2022-09-29 20:18:31 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+const { id, firstName, lastName, managers, responsibleFor } = employees;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  return employees.find((nome) => {
+    if (nome.firstName === employeeName) {
+      return nome;
+    }
+  });
 }
-
+console.log(getEmployeeByName('Emery'));
 module.exports = getEmployeeByName;
