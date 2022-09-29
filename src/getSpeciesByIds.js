d6f6d8a952d8abcbe22296a2588c3a1fcd3bc64f
Bruna <brunaalmeida075@gmail.com>
2022-09-29 17:49:11 -0300

    tirei o console

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -16,6 +16,5 @@ function getSpeciesByIds(ids, ...rest) {
   });
   return ficha;
 }
-console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
