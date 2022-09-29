fa56bdf5b1b12e5cf97ee910ef86fc06c16f6e1a
Bruna <brunaalmeida075@gmail.com>
2022-09-29 18:53:28 -0300

    resolução 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,11 +4,11 @@ const { species } = data;
 
 const [id, name, popularity, location, availability, residents] = species;
 
-function getSpeciesByIds(ids, ...rest) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
   let ficha = [];
   species.forEach((animal) => {
-    if (ids === animal.id) {
+    if (ids[0] === animal.id || ids[1] === animal.id) {
       ficha.push(animal);
     } if (ids === '') {
       return [];
@@ -16,5 +16,5 @@ function getSpeciesByIds(ids, ...rest) {
   });
   return ficha;
 }
-
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'))
 module.exports = getSpeciesByIds;
