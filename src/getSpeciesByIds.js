6c9ba01021fc58c7f96a734bd9d2e2a5586177d8
Bruna <brunaalmeida075@gmail.com>
2022-09-29 17:44:49 -0300

    resolução 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,21 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+const { species } = data;
+
+const [id, name, popularity, location, availability, residents] = species;
+
+function getSpeciesByIds(ids, ...rest) {
   // seu código aqui
+  let ficha = [];
+  species.forEach((animal) => {
+    if (ids === animal.id) {
+      ficha.push(animal);
+    } if (ids === '') {
+      return [];
+    }
+  });
+  return ficha;
 }
-console.log(data)
+console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
