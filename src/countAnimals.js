9da8b7a855b608d91190754395d82ebaa1bd5599
Bruna <brunaalmeida075@gmail.com>
2022-09-30 18:06:28 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -5,13 +5,22 @@ const { species } = data;
 function countAnimals(animal) {
   // seu código aqui
   if (!animal) {
-    const o = {};
+    const especieResidente = {};
     species.forEach((animais) => {
-      o[animais.name] = animais.residents.length;
+      especieResidente[animais.name] = animais.residents.length;
     });
-    return o;
+    return especieResidente;
+  }
+  const l = Object.keys(animal);
+  if (l.length === 1) {
+    const especie = species.find((elemento) => elemento.name === animal.specie);
+    return especie.residents.length;
+  }
+  if (l.length === 2) {
+    const especie = species.find((elemento) => elemento.name === animal.specie && elemento.sex === animal.sex);
+    return especie.residents.length;
   }
 }
-console.log(countAnimals());
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
