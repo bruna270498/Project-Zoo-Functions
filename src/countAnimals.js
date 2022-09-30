5480b385073d7ab0804013d72d4879ef61b6451b
Bruna <brunaalmeida075@gmail.com>
2022-09-30 16:20:29 -0300

    requisito 5 1 parametro

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const o = {};
+    species.forEach((animais) => {
+      o[animais.name] = animais.residents.length;
+    });
+    return o;
+  }
 }
+console.log(countAnimals());
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -19,5 +19,4 @@ function getRelatedEmployees(managerId) {
     return acc;
   }, []);
 }
-// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
