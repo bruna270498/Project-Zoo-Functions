d8cf92a2de7889a2f411b5568ac267fabe9eaa8e
Bruna <brunaalmeida075@gmail.com>
2022-10-02 09:17:50 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-}
-// console.log(countEntrants({ age: 1 }));
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const { prices } = data;
 
+function countEntrants(entrants) {
+  // const entrants = [{ age: 5 }, { age: 5 }, { age: 5 }, { age: 18 }, { age: 18 }, { age: 50 }];
+  const criança = entrants.filter((visitante) => visitante.age < 18);
+  const adulto = entrants.filter((visitante) => visitante.age >= 18 && visitante.age < 50);
+  const senior = entrants.filter((visitante) => visitante.age >= 50);
+
+  return { adult: adulto.length, child: criança.length, senior: senior.length };
+}
+function calculateEntry(entrants) {
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const adulto = countEntrants().adult;
+  const filho = countEntrants().child;
+  const senio = countEntrants().senior;
+  const total = (adulto * prices.adult) + (filho * prices.child) + (senio * prices.senior);
+  return total;
+}
 module.exports = { calculateEntry, countEntrants };
